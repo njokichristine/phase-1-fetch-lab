196d136f9287569f30d63617522d365d69b0bbab
njokichristine <christinenjoki6900@gmail.com>
2022-12-07 08:52:29 +0300

    done

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
-
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
+    / fetch('https://anapioficeandfire.com/api/books')
+    .then(res=> res.json())
+    .then(books => books.forEach(book =>{
+      renderBooks(book)
+    }))
+  }
+  function renderBooks(books) {
+    const main = document.querySelector('main');
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+      h2.innerHTML = book.name;
+      main.appendChild(h2);
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    fetchBooks();
   });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
